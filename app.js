1a1b407aa483ba08720af1719d2086097b975505
Kévin Colonjard <kevincj@outlook.fr>
2021-12-17 09:09:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const { DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+// Connect to db
+mongoose
+  .connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.ah27n.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`)
+  .then(() => console.log('Connected to db'))
+  .catch((error) => console.log('Failed to connect to db', error));
+
+// Base middleware
+app.use(express.json());
+
+module.exports = app;
