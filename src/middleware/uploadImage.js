bc7a27b40130e10954d74f3078497104804a6125
Kévin Colonjard <kevincj@outlook.fr>
2021-12-29 16:08:20 +0100

    feat: add multer middleware

---
+++ src/middleware/uploadImage.js
@@ -0,0 +1,13 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'public/images');
+  },
+  filename: (req, file, callback) => {
+    callback(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+module.exports = multer({ storage }).single('image');
