abee7ba4c3ccce54d3b27b16b7bf47285df961ee
Kévin Colonjard <kevincj@outlook.fr>
2022-01-03 14:02:43 +0100

    feat: update auth middleware

--- src/middleware/requireAuth.js
+++ src/middleware/requireAuth.js
@@ -4,10 +4,12 @@ const createError = require('http-errors')
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]
-    jwt.verify(token, process.env.TOKEN_SECRET)
+    const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET)
+
+    req.userId = decodedToken.userId
 
     next()
   } catch (err) {
-    next(createError(401, 'unauthorized request'))
+    next(createError.Unauthorized())
   }
 }
